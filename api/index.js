496be60a707fd3c5335a506bd5512a70d6c5d2d7
andricks <aestradaa10@Miumg.edu.gt>
2024-06-03 12:08:00 -0600

    TEST1

--- api/index.js
+++ api/index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require("dotenv").config();
 const cors = require('cors');
 
 const app = express();
 const PORT = 4000;
-
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Conexión a MongoDB Atlas
-const uri = 'mongodb+srv://aestradaa10:8C1lW1hktkdWvCda@database.2bnxshh.mongodb.net/?retryWrites=true&w=majority&appName=database';
+// Set the strictQuery option
+mongoose.set('strictQuery', true); // or false, depending on your preference
 
-mongoose.connect(uri, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Conectado a la base de datos');
-}).catch((err) => {
-    console.log('Error al conectar a la base de datos:', err);
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 // Rutas
 const itemsRoute = require('./routes/items');
